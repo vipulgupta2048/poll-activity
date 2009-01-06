f215d32a808691277c90a3595fd23435f98e698a
Aleksey Lim <alsroot@member.fsf.org>
2009-01-06 02:48:34 +0000

    use system gettext dir

--- i18n.py
+++ i18n.py
@@ -71,8 +71,8 @@ class LangDetails (object):
 
     def guess_translation (self, fallback=False):
         self.gnutranslation = gettext.translation(
-            'org.worldwideworkshop.PollBuilder', 'locale', 
-            [self.code], fallback=fallback)
+            'org.worldwideworkshop.PollBuilder',
+            languages=[self.code], fallback=fallback)
 
     def install (self):
         self.gnutranslation.install()
